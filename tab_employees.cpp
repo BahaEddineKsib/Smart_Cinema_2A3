7be3704d730ff3a56d8202afa72c769327976ac1
y-essine <prodtf@gmail.com>
2020-11-20 14:06:20 +0100

    Fixed Add Employee

--- tab_employees.cpp
+++ tab_employees.cpp
@@ -11,14 +11,15 @@ tab_employees::tab_employees(Ui::MainWindow *ui)
 
 void tab_employees::add_employee()
 {
-        QString name = ui->Employee_new_name->text();
+
         QString email = ui->Employee_new_email->text();
         QString username = ui->Employee_new_username->text();
         QString password = ui->Employee_new_password->text();
+        QString name = ui->Employee_new_name->text();
 
-        if(username=="" || password=="")
+        if(name=="" || email=="" || username=="" || password=="")
         {
-            qDebug("Please fill out both forms.");
+            qDebug("Please fill out all forms.");
         }
 
         else if(database::get()->db.open())
@@ -26,20 +27,23 @@ void tab_employees::add_employee()
 
             qDebug("CONNECTED TO DB.");
             QSqlQuery qry;
-            qry.prepare("INSERT INTO users (name,email,username,password) VALUES (:email,:name,:username,:password");
+            qry.prepare("INSERT INTO users (email,username,password,name,role) VALUES (:email,:username,:password,:name,:role)");
+
 
             qry.bindValue(":email",email);
-            qry.bindValue(":name",name);
             qry.bindValue(":username",username);
             qry.bindValue(":password",password);
+            qry.bindValue(":name",name);
+            qry.bindValue(":role",0);
 
-
-            ui->Employee_new_name->setText("");
             ui->Employee_new_email->setText("");
             ui->Employee_new_username->setText("");
             ui->Employee_new_password->setText("");
+            ui->Employee_new_name->setText("");
 
             qry.exec();
+            //qDebug() << qry.lastError().text();
+            qDebug("Registered");
         }
         else
         {
